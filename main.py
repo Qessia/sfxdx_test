beaef2e2890055223ed7e5fba85d992a735bc609
Timur <timurgayazovfw@gmail.com>
2024-02-21 01:57:21 +0700

    Implemented baseline (PDF Parse, FAISS and simple chain with QA prompt

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import argparse
 import sys
 import io
 import requests
+import os
 from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import GPT4All
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough, RunnablePick
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Windows; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
 
@@ -12,21 +22,58 @@ class AnswerSystem:
         self.link = link
         self.quest = quest
         self.mode = mode
+
+        self.llm = GPT4All(
+            model="model/gpt4all-falcon-newbpe-q4_0.gguf",
+            max_tokens=2048,
+        )
     
     def answer(self):
-        print(f'Well, at least I got args: \nLink: {self.link}\nQuestion: {self.quest}\nMode: {self.mode}')
-        self.load_pdf()
-    
-    def load_pdf(self):
-        response = requests.get(url=self.link, headers=headers, timeout=120)
-        on_fly_mem_obj = io.BytesIO(response.content)
+        self.load_pdf_to_FAISS()
+        self.make_chain()
 
-        reader = PdfReader(on_fly_mem_obj)
-        number_of_pages = len(reader.pages)
-        page = reader.pages[0]
-        text = page.extract_text()
-        with open('test.txt', 'w', encoding='utf-8') as f:
-            f.write(text)
+    def load_pdf_to_FAISS(self):
+        response = requests.get(url=self.link, headers=headers, timeout=120)
+        with open('temp.pdf', 'wb') as f:
+            f.write(response.content)
+
+        loader = PyPDFLoader("temp.pdf")
+        pages = loader.load_and_split()
+        if os.path.exists("temp.pdf"):
+            os.remove("temp.pdf")
+
+        self.db = FAISS.from_documents(documents=pages, embedding=GPT4AllEmbeddings())
+
+    def make_chain(self):
+        # Prompt
+        # prompt = PromptTemplate.from_template(
+        #     "Summarize the main themes in these retrieved docs: {docs}"
+        # )
+
+        # Chain
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
+
+        # chain = {"docs": format_docs} | prompt | self.llm | StrOutputParser()
+
+        # Run
+        docs = self.db.similarity_search(self.quest)[:2]
+        # chain.invoke(docs)
+        #///////////////////////////
+        rag_prompt = hub.pull("rlm/rag-prompt")
+        # rag_prompt.messages
+
+        # Chain
+        chain = (
+            RunnablePassthrough.assign(context=RunnablePick("context") | format_docs)
+            | rag_prompt
+            | self.llm
+            | StrOutputParser()
+        )
+
+        # Run
+        res = chain.invoke({"context": docs, "question": self.quest})
+        print(res)
 
 
 def main(link, quest, mode):
