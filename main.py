42c656a2eeae0489cd2736e57a4e1937a4895a71
Timur <timurgayazovfw@gmail.com>
2024-02-22 02:36:06 +0700

    added threshold for question functionality, did some refactoring

--- main.py
+++ main.py
@@ -2,59 +2,106 @@ import argparse
 import sys
 import requests
 import os
-from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import GPT4All
 from langchain_core.output_parsers import StrOutputParser
-from langchain import hub
-from langchain_core.runnables import RunnablePassthrough, RunnablePick
+from langchain_core.runnables import RunnablePassthrough
 from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Windows; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
 
 
 class AnswerSystem:
-    def __init__(self, link, quest, mode):
+    """
+    Retriever system class for answering questions in PDF text context
+    """
+    def __init__(self, link: str, quest: str, mode: str):
+        """Initial method
+
+        Args:
+            link (str): URL of PDF you want to get context from
+            quest (str): your question
+            mode (str): Embeddings mode - whether to launch them on CPU or GPU
+        """
+
         self.link = link
         self.quest = quest
-        self.mode = 'cuda' if mode == 'gpu' else mode
+        self.mode = 'cuda' if mode == 'gpu' else mode # torch uses word 'cuda' instead of 'gpu'
 
+        # Uploading LLM from GPT4All
         self.llm = GPT4All(
             model="model/gpt4all-falcon-newbpe-q4_0.gguf",
             max_tokens=2048,
         )
     
     def answer(self):
-        self.load_pdf_to_FAISS()
-        self.make_chain()
+        """method to answer the question"""
 
-    def load_pdf_to_FAISS(self):
+        self._load_pdf_to_FAISS()
+        if self.__question_is_valid(): # validate if question is related to PDF context
+            reply = self._invoke_chain()
+        else:
+            reply = 'No data to answer the question'
+        print(reply)
+
+    def _load_pdf_to_FAISS(self):
+        """Internal method to load context from PDF to FAISS database using Embeddings"""
+
+        # PDF parsing
         response = requests.get(url=self.link, headers=headers, timeout=120)
+
+        # PyPDFLoader doesn't work with URLs, so we save it
         with open('temp.pdf', 'wb') as f:
             f.write(response.content)
 
         loader = PyPDFLoader("temp.pdf")
-        pages = loader.load_and_split()
+        pages = loader.load()
+
+        # splitting text of documents to chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, # n_symbols
+            chunk_overlap=20 # n_symbols shared by chunks
+        )
+        pages = text_splitter.split_documents(pages)
+
         if os.path.exists("temp.pdf"):
             os.remove("temp.pdf")
 
+        # Loading embeddings with specified device
         embeddings = HuggingFaceEmbeddings(
             model_name="sentence-transformers/all-mpnet-base-v2",
             model_kwargs= {'device': self.mode},
             encode_kwargs={'normalize_embeddings': True}
         )
 
-        self.db = FAISS.from_documents(documents=pages, embedding=embeddings).as_retriever(search_type="mmr", search_kwargs={"score_threshold": 0.5, "k": 1})
+        self.db = FAISS.from_documents(documents=pages, embedding=embeddings)
 
-    def make_chain(self):
+    def __question_is_valid(self) -> bool:
+        """Internal method to determine whether question is close enoung to PDF context
 
-        # docs, scores = zip(*self.db.similarity_search_with_score(self.quest))
-        # print(scores)
-        docs = self.db.get_relevant_documents(self.quest)
+        Returns:
+            bool: True if question is valid, else False
+        """
 
+        threshold = 1.6 # experimentally determined value
+        docs_with_scores = self.db.similarity_search_with_score(self.quest) # returns list of tuple[Document, float]
+        return docs_with_scores[0][1] < threshold # [0] index - doc with best (lowest) score, [1] index - score
+
+    def _invoke_chain(self):
+        """
+        Internal method to invoke chain with retrieval prompt
+        """
+
+        # Switch FAISS db to retriever mode
+        self.db = self.db.as_retriever(
+            search_type="mmr", # Maximum Marginal Retrieval score
+            search_kwargs={"k": 1} # Num of docs returned
+        )
+
+        # Q&A prompt
         template = """Answer the question based only on the following context:
         {context}
 
@@ -69,8 +116,7 @@ class AnswerSystem:
             | StrOutputParser()
         )
 
-        res = chain.invoke(self.quest)
-        print('ANSWER:', res)
+        return f'ANSWER:\n{chain.invoke(self.quest)}'
 
 
 def main(link, quest, mode):
